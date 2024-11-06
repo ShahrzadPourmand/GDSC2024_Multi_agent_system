5db801f0cdb2a082c4b4d34e4460a2f9d33a8233
Shahrzad <11048793+ShahrzadPourmand@users.noreply.github.com>
2024-11-06 11:56:47 +0100

    Create tables.py

---
+++ submission/tools/tables.py
@@ -0,0 +1,267 @@
+pirls_trends_avg_reading = {
+    "Australia": {
+        "2021 (During COVID-19)": 540,
+        "2016": 544,
+        "2011": 527,
+    },
+    "Austria": {
+        "2021 (During COVID-19)": 530,
+        "2016": 541,
+        "2011": 529,
+        "2006": 538
+    },
+    "Azerbaijan": {
+        "2021 (During COVID-19)": 440,
+        "2016": 472,
+        "2011": 462,
+    },
+    "Belgium (Flemish)": {
+        "2021 (During COVID-19)": 511,
+        "2016": 525,
+        "2006": 547,
+    },
+    "Belgium (French)": {
+        "2021 (During COVID-19)": 494,
+        "2016": 497,
+        "2011": 506,
+        "2006": 500
+    },
+    "Bulgaria": {
+        "2021 (During COVID-19)": 540,
+        "2016": 552,
+        "2011": 532,
+        "2006": 547,
+        "2001": 550
+    },
+    "Chinese Taipei": {
+        "2021 (During COVID-19)": 544,
+        "2016": 559,
+        "2011": 553,
+        "2006": 535
+    },
+    "Cyprus": {
+        "2021 (During COVID-19)": 511,
+        "2001": 494,
+    },
+    "Czech Republic": {
+        "2021 (During COVID-19)": 540,
+        "2016": 543,
+        "2011": 545,
+        "2001": 537
+    },
+    "Denmark": {
+        "2021 (During COVID-19)": 539,
+        "2016": 547,
+        "2011": 554,
+        "2006": 546
+    },
+    "Egypt": {
+        "2021 (During COVID-19)": 378,
+        "2016": 330,
+    },
+    "England": {
+        "2021 (During COVID-19)": 558,
+        "2016": 559,
+        "2011": 552,
+        "2006": 539,
+        "2001": 553
+    },
+    "Finland": {
+        "2021 (During COVID-19)": 549,
+        "2016": 566,
+        "2011": 568,
+    },
+    "France": {
+        "2021 (During COVID-19)": 514,
+        "2016": 511,
+        "2011": 520,
+        "2006": 522,
+        "2001": 525
+    },
+    "Germany": {
+        "2021 (During COVID-19)": 524,
+        "2016": 537,
+        "2011": 541,
+        "2006": 548,
+        "2001": 539
+    },
+    "Hong Kong SAR": {
+        "2021 (During COVID-19)": 573,
+        "2016": 569,
+        "2011": 571,
+        "2006": 564,
+        "2001": 528
+    },
+    "Iran, Islamic Rep. of": {
+        "2021 (During COVID-19)": 413,
+        "2016": 428,
+        "2011": 457,
+        "2006": 421,
+        "2001": 414
+    },
+    "Israel": {
+        "2021 (During COVID-19)": 510,
+        "2016": 530,
+        "2011": 541,
+    },
+    "Italy": {
+        "2021 (During COVID-19)": 537,
+        "2016": 548,
+        "2011": 541,
+        "2006": 551,
+        "2001": 541
+    },
+    "Macao SAR": {
+        "2021 (During COVID-19)": 536,
+        "2016": 546,
+    },
+    "Netherlands": {
+        "2021 (During COVID-19)": 527,
+        "2016": 545,
+        "2011": 546,
+        "2006": 547,
+        "2001": 554
+    },
+    "New Zealand": {
+        "2021 (During COVID-19)": 521,
+        "2016": 523,
+        "2011": 531,
+        "2006": 532,
+        "2001": 529
+    },
+    "North Macedonia": {
+        "2021 (During COVID-19)": 442,
+        "2006": 442,
+        "2001": 442
+    },
+    "Norway": {
+        "2021 (During COVID-19)": 539,
+        "2016": 559,
+    },
+    "Oman": {
+        "2021 (During COVID-19)": 429,
+        "2016": 418,
+        "2011": 391,
+    },
+    "Poland": {
+        "2021 (During COVID-19)": 549,
+        "2016": 565,
+    },
+    "Portugal": {
+        "2021 (During COVID-19)": 520,
+        "2016": 528,
+        "2011": 541,
+    },
+    "Russian Federation": {
+        "2021 (During COVID-19)": 567,
+        "2016": 581,
+        "2011": 568,
+        "2006": 565,
+        "2001": 528
+    },
+    "Singapore": {
+        "2021 (During COVID-19)": 587,
+        "2016": 576,
+        "2011": 567,
+        "2006": 558,
+        "2001": 528
+    },
+    "Slovak Republic": {
+        "2021 (During COVID-19)": 529,
+        "2016": 535,
+        "2011": 535,
+        "2006": 531,
+        "2001": 518
+    },
+    "Slovenia": {
+        "2021 (During COVID-19)": 520,
+        "2016": 542,
+        "2011": 530,
+        "2006": 522,
+        "2001": 502
+    },
+    "South Africa": {
+        "2021 (During COVID-19)": 288,
+        "2016": 320,
+    },
+    "Spain": {
+        "2021 (During COVID-19)": 521,
+        "2016": 528,
+        "2011": 513,
+        "2006": 513
+    },
+    "Sweden": {
+        "2021 (During COVID-19)": 544,
+        "2016": 555,
+        "2011": 542,
+        "2006": 549,
+        "2001": 561
+    },
+    "Turkiye": {
+        "2021 (During COVID-19)": 496,
+        "2001": 449
+    },
+}
+
+
+########################################################
+
+pirls_countries_2021_gni_per_capita = {
+    "Albania": 5210,
+    "Australia": 53680,
+    "Austria": 48360,
+    "Azerbaijan": 4480,
+    "Bahrain": 19900,
+    "Belgium (Flemish and French)": 45800,
+    "Brazil": 7850,
+    "Bulgaria": 9630,
+    "Canada": 43540,
+    "Chinese Taipei": 29153,
+    "Croatia": 14530,
+    "Cyprus": 26440,
+    "Czech Republic": 22070,
+    "Denmark": 63010,
+    "Egypt": 3000,
+    "England": 39830,
+    "Finland": 49800,
+    "France": 39500,
+    "Georgia": 4260,
+    "Germany": 47520,
+    "Hong Kong SAR": 48630,
+    "Hungary": 15890,
+    "Iran": 2960,
+    "Ireland": 65750,
+    "Israel": 42610,
+    "Italy": 32360,
+    "Jordan": 4310,
+    "Kazakhstan": 8710,
+    "Kosovo": 4440,
+    "Latvia": 17890,
+    "Lithuania": 19620,
+    "Macao SAR": 75690,
+    "Malta": 25860,
+    "Montenegro": 7900,
+    "Morocco": 3020,
+    "Netherlands": 51070,
+    "New Zealand": 42870,
+    "North Macedonia": 5750,
+    "Northern Ireland": 39830,
+    "Norway": 78290,
+    "Oman": 15030,
+    "Poland": 15260,
+    "Portugal": 21810,
+    "Qatar": 55990,
+    "Russian Federation": 10690,
+    "Saudi Arabia": 21930,
+    "Serbia": 7430,
+    "Singapore": 64010,
+    "Slovak Republic": 18920,
+    "Slovenia": 25340,
+    "South Africa": 6010,
+    "Spain": 27360,
+    "Sweden": 54050,
+    "Turkiye": 9050,
+    "United Arab Emirates": 39410,
+    "United States": 64140,
+    "Uzbekistan": 1740
+}
